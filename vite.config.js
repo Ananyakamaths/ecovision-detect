d0128530382193d5ae4f22b8e37f8187743af8f4
ananyakamaths15 <ananyakamaths.15@gmail.com>
2026-04-19 15:23:24 +0530

    success

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,6 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const base = process.env.VITE_BASE || '/'
-
-// https://vite.dev/config/
 export default defineConfig({
-  base,
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api/save': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      },
-    },
-  },
-  build: {
-    chunkSizeWarningLimit: 1600,
-  },
 })
-=======
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-});
->>>>>>> 29d9d301065f9998dcd4bd997d110d0fd578b535
